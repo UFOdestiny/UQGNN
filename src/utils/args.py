a42c50c935860da960fb3c97e8e24e3a67a06f79
UFOdestiny <UFOdestiny@gmail.com>
2024-08-01 12:14:03 -0400

    first commit

---
+++ src/utils/args.py
@@ -0,0 +1,20 @@
+import argparse
+def get_public_config():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--device', type=str, default="cuda")
+    parser.add_argument('--dataset', type=str, default='Shenzhen')
+    parser.add_argument('--years', type=str, default='2019')
+    parser.add_argument('--model_name', type=str, default='')
+    parser.add_argument('--seed', type=int, default=2024)
+    parser.add_argument('--bs', type=int, default=64)
+    parser.add_argument('--seq_len', type=int, default=12)
+    parser.add_argument('--horizon', type=int, default=1)
+    parser.add_argument('--feature', type=int, default=5)
+    parser.add_argument('--input_dim', type=int, default=5)
+    parser.add_argument('--output_dim', type=int, default=5)
+    parser.add_argument('--mode', type=str, default='train')
+    parser.add_argument('--max_epochs', type=int, default=2000)
+    parser.add_argument('--patience', type=int, default=500)
+    parser.add_argument('--normalize', type=bool, default=True)
+    parser.add_argument('--uq_metrics', type=bool, default=False)
+    return parser
